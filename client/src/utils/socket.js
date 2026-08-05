66e16df5344f50d93274f208874f3df87ba04f36
Nwosu Emmanuella Oluebube <ellanwosu29@gmail.com>
2026-08-05 17:57:50 +0100

    Add missing client files including package.json

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,10 +1,11 @@
 import { io } from 'socket.io-client';
 
 const SOCKET_URL = 'http://localhost:5000';
+// import { API_BASE_URL } from '../config';
 
 const socket = io(SOCKET_URL, {
-  autoConnect: false,
-  withCredentials: true,
+    autoConnect: false,
+    withCredentials: true,
 });
 
 export default socket;
